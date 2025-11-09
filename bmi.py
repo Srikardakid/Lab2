770b3667472c1598ba56ff3bcdd48f24c7dde807
Srikar <saisrikar.24@ichat.sp.edu.sg>
2025-11-09 11:47:32 +0800

    Returning function

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height,weight):
     print("BMI = "+ str(bmi))
     if(bmi < 18.5):
         print("You are underweight")
+        return -1
     else:
         if(bmi<25.0):
             print("You are normal weight")
+            return 0
         else:
             print("You are overweight")
+            return 1
 calculate_bmi(1.77,59)
